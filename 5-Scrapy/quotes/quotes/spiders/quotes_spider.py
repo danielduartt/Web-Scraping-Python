d5af476e1fa0f2c44d1730a7bb4cf71a7a0c5d15
Daniel Duarte <dev.daniel.duarte@gmail.com>
2023-09-15 17:59:17 -0300

    adicionando melhorias

--- 5-Scrapy/quotes/quotes/spiders/quotes_spider.py
+++ 5-Scrapy/quotes/quotes/spiders/quotes_spider.py
@@ -1,18 +1,32 @@
 import scrapy
 
+def clean_text(text):
+    text = text.strip(u'\u201c')
+    text = text.strip(u'\u201d')
+    return text
+
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
-    start_urls = ['https://quotes.toscrape.com/page/1/',
-                  'https://quotes.toscrape.com/page/2/']
+    start_urls = ['https://quotes.toscrape.com/page/1/']
 
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
-                "text": quote.css('span.text::text').get(),
-                "author": quote.css('small.author::text').get(),
+                "text": clean_text(quote.css('span.text::text').get()),
+                "author": clean_text(quote.css('small.author::text').get()),
                 "tag": quote.css('div.tags a.tag::text').getall()
             }
+
+        next_page = response.css('li.next a::attr(href)').get() #pega os links relativos
+        if next_page is not None: #se existir, vai executar o que está dentro do if
+            yield response.follow(next_page, callback=self.parse) #callback = a um método de classe
+
+
+
+
 
 
 #scrapy crawl (nome dado a spider)
 #scape crawl -o quotes.json
+#response.css('li.next a::attr(href)').get() pegar o link do href
+
